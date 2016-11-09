0f9890e41b9bec70a8a437c210196d9cc91973cb
Andreas Eberhard <andreas.eberhard@vanoli-ag.ch>
2016-11-09 01:02:28 +0100

    ADD: Custom evt listener for Mobilenav btn

--- js/_dev/main.js
+++ js/_dev/main.js
@@ -1,7 +1,7 @@
 const idsJsModule = (function ($, ids) {
   'use strict';
-  ids.sticky = function (e, t) { // element[string], target[obj]
-    t.bind('load', function(){
+  ids.sticky = (e, t) => { // element[string], target[obj]
+    t.bind('load', () => {
       let eHeight = 0, 
           eTop = 0, 
           $e = $(e);
@@ -11,8 +11,6 @@ const idsJsModule = (function ($, ids) {
       function pos() {
         eHeight = $e.height();
         eTop = (t.scrollTop()+t.height()-eHeight)+'px';
-        console.log($(document.body).height());
-        console.log(t.height());
         if ( ($(document.body).height()) < t.height()) {
           $e.css({
             position: "absolute"
@@ -28,8 +26,12 @@ const idsJsModule = (function ($, ids) {
       
       $(window).scroll(pos).resize(pos);
       
-    }); // end of bind
-  }; // end of ids.sticky
+    } // end of bind
+  } // end of ids.sticky
+  
+  ids.nav = (obj) => {
+    // none
+  } // end of ids.nav
   return ids;
 }(jQuery, idsJsModule || {}));
 
@@ -37,6 +39,11 @@ const idsJsModule = (function ($, ids) {
   'use strict';
   $(document).ready(function () {
     idsJsModule.sticky('#footerline',$(window));
+    document.getElementById(obj).addEventListener('touchend', (e) => {
+      e.preventDefault();
+      var touch = e.touches[0];
+      console.log('Test', touch.pageX + " - " + touch.pageY);
+    }, false);
   });
 }(jQuery, idsJsModule));
 
